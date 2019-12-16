eb01c98f83764a53259935371c6ff78ba7cde857
Robin.Habran <robinhabran@gmail.com>
2019-12-16 12:29:27 +0100

    fin de partie 7

--- partie7/exercice6/index.php
+++ partie7/exercice6/index.php
@@ -27,14 +27,6 @@
         }
         return $bool;
     }
-    /**
-     * fonction qui renvoi une phrase dans le cas d'invalidité de la valeur du form
-     * @param string $inputVal
-     * @return string
-     */
-    function inputValIncorrect($inputVal) {
-        return 'Veuillez renseigner le champ ' . $inputVal;
-    }
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -51,33 +43,39 @@
                 <h2>Renseignez vos coordonnées</h2>
                 <div id="containerFormFront">
                     <?php
-                    if (inputFieldCorrect($_POST['inputFirstname']) && inputFieldCorrect($_POST['inputLastname'])) {
+                    if (isset($_POST['submitButton']) && inputFieldCorrect($_POST['inputFirstname']) && inputFieldCorrect($_POST['inputLastname'])) {
                         echo 'Vos données ont été renseignées avec succès.';
                     } else {
-                        if (){
-                            if(){
-                                echo inputValIncorrect($_POST['inputFirstname']);
-                            }else{
-                                echo inputValIncorrect($_POST['inputLastname']);
+                        ?>
+                        <form class="myForm" action="#" method="POST">
+                            <div class="choiceSelect">
+                                <label for="choiceCivility">Civilité : </label>
+                                <select name="choiceCivility" id="choiceCivility" value="<?php isset($_POST['choiceCivility']) ?>">
+                                    <option disabled selected>à choisir</option>
+                                    <option value="mme">Madame</option>
+                                    <option value="mr">Monsieur</option>
+                                </select>
+                            </div>
+                            <label for="inputFirstname">Prénom :</label>
+                            <input type="text" name="inputFirstname" placeholder="Jean" id="inputFirstname" value="<?php isset($_POST['inputFirstname']) ?>"/>
+                            <label for="inputLastname">Nom :</label>
+                            <input type="text" name="inputLastname" placeholder="Mich" id="inputLastname" value="<?php isset($_POST['inputLastname']) ?>" />
+                            <input type="submit" name="submitButton" id="submitButton" value="S'enregistrer" />
+                        </form>
+                        <?php
+                        if(isset($_POST['submitButton'])){
+                            // case où les champs sont incorrect
+                            if(!inputFieldCorrect($_POST['inputFirstname']) && !inputFieldCorrect($_POST['inputLastname'])){
+                                ?><p class="error"><?= 'Veuillez remplir le champ Prénom';?></p><?php
+                                ?><p class="error"><?= 'Veuillez remplir le champ Nom';?></p><?php
+                            // cas où le champ Firstname est incorrect
+                            }else if(!inputFieldCorrect($_POST['inputFirstname'])){
+                                ?><p class="error"><?= 'Veuillez remplir le champ Prénom avec des lettres';?></p><?php
+                            // cas où le champ Lastname est incorrect
+                            }else if(!inputFieldCorrect($_POST['inputLastname'])){
+                                ?><p class="error"><?= 'Veuillez remplir le champ Nom';?></p><?php
                             }
-                        }else{
-                            ?>
-                            <form class="myForm" action="#" method="POST">
-                                <div class="choiceSelect">
-                                    <label for="choiceCivility">Civilité : </label>
-                                    <select name="choiceCivility" id="choiceCivility" value="<?php isset($_POST['choiceCivility']) ?>">
-                                        <option disabled selected>à choisir</option>
-                                        <option value="mme">Madame</option>
-                                        <option value="mr">Monsieur</option>
-                                    </select>
-                                </div>
-                                <label for="inputFirstname">Prénom :</label>
-                                <input type="text" name="inputFirstname" placeholder="Jean" id="inputFirstname" value="<?php isset($_POST['inputFirstname']) ?>"/>
-                                <label for="inputLastname">Nom :</label>
-                                <input type="text" name="inputLastname" placeholder="Mich" id="inputLastname" value="<?php isset($_POST['inputLastname']) ?>" />
-                                <input type="submit" id="submitButton" value="S'enregistrer" />
-                            </form>
-                            <?php
+                        }
                     }
                     ?>
                 </div>
--- partie7/exercice7/index.php
+++ partie7/exercice7/index.php
@@ -1,27 +1,56 @@
-<?php 
-    // déclaration variables //
-    $regexName = '/[a-zéèà\-]/gmi';
-    // fin déclaration //
-    // fonctions //
-    function acceptRegexName($inputVal){
-        $bool = (preg_match($inputVal, $regexName)) ? true : false;
-        return $bool;
-    }
-    function inputFieldCorrect($inputId) {
-        $bool = (!empty($_POST[$inputId]) && acceptRegexName($inputId)) ? true : false;
-        return $bool;
-    }
-    function submitForm(){
-        header('Location: user.php');
-    }
-    function inputValIncorrect($inputId) {
-        echo 'Veuillez renseigner le champ ' . $inputId;
-    }
-    function submitFormCorrect($inputId){
-        (inputFieldCorrect($inputId)) ? submitForm() : exit;
-    }
-?>
 <!DOCTYPE html>
+<?php
+    // fonctions
+        /**
+         * fonction de comparaison de la valeur du POST avec la regex
+         * @param string $inputVal
+         * @return boolean
+         */
+        function acceptRegexName($inputVal) {
+            $regexName = '/^[a-zéèà\- ]+$/i';
+            $bool = (preg_match($regexName, $inputVal));
+            return $bool;
+        }
+        /**
+         * fonction qui vérifie si la valeur du POST est correct
+         * @param string $inputVal
+         * @return boolean 
+         */
+        function inputFieldCorrect($inputVal) {
+            $bool = false;
+            if (isset($inputVal)){
+                if(acceptRegexName($inputVal)){
+                    $bool = true;
+                }
+            }
+            return $bool;
+        }
+        // vérification de l'upload du fichier
+        function verifFile($inputFile) {
+            $infoFile = pathinfo($_FILES[$inputFile]['name']);
+            $extensionFile = $infoFile['extension'];
+            $extensionsAllowed = array('jpg', 'jpeg', 'gif', 'png');
+            // vérification de l'upload du fichier
+            if (isset($_FILES[$inputFile]) && $_FILES[$inputFile]['error'] == 0){
+                // vérification de la taille du fichier
+                if ($_FILES[$inputFile]['size'] <= 1000000){
+                    // vérification de l'extension du fichier
+                    if (in_array($extensionFile, $extensionsAllowed)){
+                        return 1;
+                     }
+                }
+            }
+            return 0;
+        }
+    // fin déclaration de founctions
+        if(isset($_POST['submitButton'])){
+            if(isset($_FILES['inputFile']) && $_FILES['inputFile']['error'] == 0){
+                $infoFile = pathinfo($_FILES['inputFile']['name']);
+                $extensionFile = $infoFile['extension'];
+                $extensionsAllowed = array('jpg', 'jpeg', 'gif', 'png');
+            }
+        }
+?>
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -35,22 +64,64 @@
       <div id="containerFormBack">
         <h2>Renseignez vos coordonnées</h2>
         <div id="containerFormFront">
-          <form class="myForm" action="" method="POST">
-              <div class="choiceSelect">
-                <label for="choiceCivility">Civilité : </label>
-                <select name="choiceCivility" id="choiceCivility">
-                    <option disabled selected>à choisir</option>
-                    <option value="mme">Madame</option>
-                    <option value="mr">Monsieur</option>
-                </select>
-              </div>
-            <label for="inputFirstname">Prénom :</label>
-            <input type="text" name="inputFirstname" placeholder="Jean" id="inputFirstname" />
-            <label for="inputLastname">Nom :</label>
-            <input type="text" name="inputLastname" placeholder="Mich" id="inputLastname" />
-            <input type="submit" id="submitButton" value="S'enregistrer" />
-            <?php 
-                (empty($_POST['inputFirstname']))
+            <?php
+                // test d'existence d'un fichier upload
+                if (isset($_POST['submitButton']) && inputFieldCorrect($_POST['inputFirstname']) && inputFieldCorrect($_POST['inputLastname']) && verifFile('inputFile')) {
+                    // upload du fichier
+                    move_uploaded_file($_FILES['inputFile']['tmp_name'], 'uploads/' . basename($_FILES['inputFile']['name']));
+                    echo 'Vos données ont été renseignées avec succès.';
+                } else {
+                ?>  <form class="myForm" action="" method="POST" enctype="multipart/form-data">
+                    <div class="choiceSelect">
+                      <label for="choiceCivility">Civilité : </label>
+                      <select name="choiceCivility" id="choiceCivility">
+                          <option disabled selected>à choisir</option>
+                          <option value="mme">Madame</option>
+                          <option value="mr">Monsieur</option>
+                      </select>
+                    </div>
+                    <label for="inputFirstname">Prénom :</label>
+                    <input type="text" name="inputFirstname" placeholder="Jean" id="inputFirstname" />
+                    <label for="inputLastname">Nom :</label>
+                    <input type="text" name="inputLastname" placeholder="Mich" id="inputLastname" />
+                    <label for="inputFile">Votre photo de profil :</label>
+                    <input type="file" name="inputFile" id="inputFile" />
+                    <input type="submit" name="submitButton" id="submitButton" value="S'enregistrer" />
+                    <?php
+                    // si bouton de submit a été clické
+                    if(isset($_POST['submitButton'])){
+                        // cas du champ upload vide
+                        if (!isset($_FILES['inputFile']) && $_FILES['inputFile']['error'] != 0){
+                            ?><p class="error"><?php echo 'Veuillez upload une photo de profil';
+                        }
+                        // cas du fichier upload trop lourd
+                        if (isset($_FILES['inputFile']) && $_FILES['inputFile']['error'] == 0){
+                            ?><p class="error"><?php echo 'Veuillez upload une photo de profil';
+                            if ($_FILES['inputFile']['size'] > 1000000){
+                                ?><p class="error"><?php echo 'Votre image est trop volumineuse, 1 Mo maximum autorisé';
+                            }
+                        }
+                        // cas du fichier upload n'est pas une image
+                        if (isset($_FILES['inputFile']) && $_FILES['inputFile']['error'] == 0){
+                            if ($_FILES['inputFile']['size'] <= 1000000){
+                                if (!in_array($extensionFile, $extensionsAllowed)){
+                                    ?><p class="error"><?php echo 'Veillez à ce que votre fichier soit une image';
+                                }
+                            }
+                        }
+                        // case où les champs sont incorrect
+                        if(!inputFieldCorrect($_POST['inputFirstname']) && !inputFieldCorrect($_POST['inputLastname'])){
+                            ?><p class="error"><?= 'Veuillez remplir le champ Prénom';?></p><?php
+                            ?><p class="error"><?= 'Veuillez remplir le champ Nom';?></p><?php
+                        // cas où le champ Firstname est incorrect
+                        }else if(!inputFieldCorrect($_POST['inputFirstname'])){
+                            ?><p class="error"><?= 'Veuillez remplir le champ Prénom avec des lettres';?></p><?php
+                        // cas où le champ Lastname est incorrect
+                        }else if(!inputFieldCorrect($_POST['inputLastname'])){
+                            ?><p class="error"><?= 'Veuillez remplir le champ Nom';?></p><?php
+                        }
+                    }
+                }
             ?>
           </form>
         </div>
